61815cbeb092dab51281c5ae5cc8276998dd5ac4
skrrrm <a@macbook-windows>
2020-02-21 09:54:07 +0100

    Add project files.

---
+++ KEAOpgave3.4/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace KEAOpgave3._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(386, 68);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(100, 31);
+            this.textBoxResult.TabIndex = 0;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(102, 53);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 25);
+            this.labelInfo.TabIndex = 1;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(456, 250);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(123, 63);
+            this.buttonCalculate.TabIndex = 2;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(107, 247);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 25);
+            this.labelResult.TabIndex = 3;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(567, 68);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 25);
+            this.labelError.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.textBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ KEAOpgave3.4/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Skriv et program, der tester brugerens evner til hovedregning.
+ * Programmet skal ved tryk på knap stille et gangestykke, 
+ * hvor faktorerne er hele tal mellem 0 og 10.
+ * Brugeren taster resultatet ind (husk, at indtastning i en textBox ikke er tal).
+ * Programmet skal angive om facit er rigtigt eller forkert. 
+ * Hvis facit er forkert skal det rigtige facit angives.
+*/
+namespace KEAOpgave3._4
+{
+    public partial class Form1 : Form
+    {
+        // Variabler
+        Random random1 = new Random();
+        Random random2 = new Random();
+        double first, second, result, doubleTextboxResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+            first = Convert.ToDouble(random1.Next(0, 10));
+            second = Convert.ToDouble(random2.Next(0, 10));
+            result = first * second;
+            labelInfo.Text = $"What is {first} multiplied by {second}?";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                doubleTextboxResult = Convert.ToDouble(textBoxResult.Text);
+            }
+            catch
+            {
+                labelError.Text = "Numbers only";
+            }
+
+            if (result == doubleTextboxResult)
+                labelResult.Text = "Correct!";
+            else if (result != doubleTextboxResult)
+                labelResult.Text = $"Incorrect! The result is {result}";
+        }
+    }
+}
